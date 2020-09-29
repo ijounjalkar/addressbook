0ac2eb735a478001f861081fde979ade526f4eea
ijounjalkar <jounjalkarisha@gmail.com>
2020-09-29 21:13:02 +0530

    UC1_details commited

--- Contact.java
+++ Contact.java
@@ -1,7 +1,62 @@
 package address;
 
 public class Contact {
-	  
-		
-
+	private String firstName;
+	private String lastName;
+	private long phoneNumber;
+	private String address;
+	private String city;
+	private String state;
+	private int zipcode;
+	private String email;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
